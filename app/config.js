a3a3f971665a0c264308d800ac44dbf264734877
Shahina Rahman <shahinarahman101@gmail.com>
2022-11-02 21:24:12 +0000

    Font size tweaks and title name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Data Products - Design History',
+  serviceName: 'Data Products Discovery - Design History',
 
   // Default port that prototype runs on
   port: '3000',
